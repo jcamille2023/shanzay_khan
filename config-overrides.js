5cfd668b1ae4193771712383c2a01d3bf8d1d550
Jossaya Camille <jcamille2023@fau.edu>
2024-06-02 01:26:37 -0400

    IT WORKS

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,29 @@
-module.exports = function override(config, env) {
-  config.module.rules.push({
-    test: /\.tsx?$/,
-    use: 'ts-loader',
-    exclude: /node_modules\/(?!template-stuff)/,
-  });
-  config.module.rules.push({
-    test: /\.jsx?$/,
-    use: 'babel-loader',
-    exclude: /node_modules\/(?!template-stuff)/,
-  });
+const { override } = require('customize-cra');
+module.exports = override(
+  (config, env) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules\/(?!template-stuff)/,
+    });
+    config.module.rules.push({
+      test: /\.jsx?$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            require.resolve('babel-preset-react-app')
+          ],
+          // This is a feature of `babel-loader` for webpack (not Babel itself).
+          // It enables caching results in ./node_modules/.cache/babel-loader/
+          // directory for faster rebuilds.
+          cacheDirectory: true,
+          cacheCompression: false,
+          compact: false,
+        },
+      },
+      exclude: /node_modules\/(?!template-stuff)/,
+    });
     return config;
-  };
+  }
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,7 +9,9 @@ import Home from './pages/Home';
 function App() {
   return (
     <Router>
-      <Route path="/" Component={Home} />
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
     </Router>
   );
 }
